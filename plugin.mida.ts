8a0a90a413438a74bf02a546a090d0ed043bc5c0
Vasile Pește <vasile.peste@protonmail.ch>
2021-12-24 00:11:50 +0100

    Update

--- plugin.mida.ts
+++ plugin.mida.ts
@@ -1,12 +1,8 @@
 import { MyPlugin } from "!/src/core/MyPlugin";
 
 export default new MyPlugin({
-    // Your plugin id, required.
-    id: "my-plugin",
-    // Your plugin name, required.
-    name: "My Plugin",
-    // Your plugin version, required.
-    version: "1.0.0",
-    // Your plugin description, optional.
-    description: "This plugin does nothing.",
+    id: "my-plugin", // Plugin id, required
+    name: "My Plugin", // Plugin name, required
+    version: "1.0.0", // Plugin version, required
+    description: "This plugin does nothing.", // Plugin description, optional
 });
