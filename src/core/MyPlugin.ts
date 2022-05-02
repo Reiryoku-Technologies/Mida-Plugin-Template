1148f1346a65cfd37ee79bab60c8581d1d49e99c
Vasile Pește <vasile.peste@protonmail.ch>
2022-05-02 13:41:24 +0200

    Cleanup

--- src/core/MyPlugin.ts
+++ src/core/MyPlugin.ts
@@ -8,7 +8,7 @@ export class MyPlugin extends MidaPlugin {
         super({
             id: "my-plugin", // Plugin id, required
             name: "My Plugin", // Plugin name, required
-            version: require("!/package.json").version, // Plugin version, required
+            version: "1.0.0", // Plugin version, required
             description: "This plugin does nothing.", // Plugin description, optional
         });
     }
